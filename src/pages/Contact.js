92a59e738b9ad8301e5d1bca2166c47c28a58198
sime-81 <yapock123@gmail.com>
2023-10-01 15:33:16 +0200

    Version final

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,12 +6,31 @@ import ContactForm from "../components/ContactForm";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import SocialNetwork from "../components/SocialNetwork";
 import Buttons from "../components/Buttons";
+import { motion } from "framer-motion";
 
 const Contact = () => {
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 200,
+    },
+  };
+
   return (
     <main>
       <Mouse />
-      <div className="contact">
+      <motion.div
+        initial="out"
+        animate="in"
+        exit="out"
+        variants={pageTransition}
+        transition={{ duration: 0.4 }}
+        className="contact"
+      >
         <Navigation />
         <Logo />
         <ContactForm />
@@ -57,7 +76,7 @@ const Contact = () => {
           </div>
         </div>
         <Buttons left={"/projet-4"} />
-      </div>
+      </motion.div>
     </main>
   );
 };
